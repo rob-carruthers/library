9a3804d329caa908956e4ccc3621e08d79ea24e7
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2023-01-14 19:22:41 +0000

    Create draft of modal for creating a book

--- script.js
+++ script.js
@@ -15,44 +15,59 @@ class Book {
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
-    const newBook = new Book(title, author, pages, isRead);
-    myLibrary.push(newBook);
+  const newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
 
-    const bookContainer = document.getElementById("bookContainer")
+  const bookContainer = document.getElementById("bookContainer");
 
-    const bookCard = document.createElement("div");
-    bookCard.classList = "bookCard";
-    bookContainer.appendChild(bookCard);
+  const bookCard = document.createElement("div");
+  bookCard.classList = "bookCard";
+  bookContainer.appendChild(bookCard);
 
-    const titleDiv = document.createElement("div");
-    const authorDiv = document.createElement("div");
-    const pagesDiv = document.createElement("div");
-    const isReadDiv = document.createElement("div");
+  const titleDiv = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const pagesDiv = document.createElement("div");
+  const isReadDiv = document.createElement("div");
 
-    titleDiv.classList = "title";
-    authorDiv.classList = "author";
-    pagesDiv.classList = "pages";
-    isReadDiv.classList = "isRead";
-    
-    bookCard.appendChild(titleDiv);
-    bookCard.appendChild(authorDiv);
-    bookCard.appendChild(pagesDiv);
-    bookCard.appendChild(isReadDiv);
+  titleDiv.classList = "title";
+  authorDiv.classList = "author";
+  pagesDiv.classList = "pages";
+  isReadDiv.classList = "isRead";
 
-    titleDiv.textContent = newBook.title;
-    authorDiv.textContent = newBook.author;
-    pagesDiv.textContent = newBook.pages + " pages";
-    if (newBook.isRead) {
-      isReadDiv.textContent = "Already read";
-    }
-    else {
-      isReadDiv.textContent = "Not read yet";
-    };
+  bookCard.appendChild(titleDiv);
+  bookCard.appendChild(authorDiv);
+  bookCard.appendChild(pagesDiv);
+  bookCard.appendChild(isReadDiv);
+
+  titleDiv.textContent = newBook.title;
+  authorDiv.textContent = newBook.author;
+  pagesDiv.textContent = newBook.pages + " pages";
+  if (newBook.isRead) {
+    isReadDiv.textContent = "Already read";
+  } else {
+    isReadDiv.textContent = "Not read yet";
+  }
 }
 
 addBookToLibrary("Hi", "John Smith", 133, false);
 addBookToLibrary("Hello World", "Aidan Smith", 153, true);
 addBookToLibrary("Another Book", "Aidan Jones", 463, true);
 
+const modal = document.getElementById("modalContainer");
+const openBookCreator = document.getElementById("openBookCreator");
+const modalCloseButton = document.getElementById("modalCloseButton");
+
+openBookCreator.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+modalCloseButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
