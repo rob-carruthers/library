dece5cb539bbb1c18cd2dfe617840f00b8f6ba3c
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2023-01-14 18:39:26 +0000

    Create layout and test books

--- script.js
+++ script.js
@@ -17,8 +17,42 @@ class Book {
 function addBookToLibrary(title, author, pages, isRead) {
     const newBook = new Book(title, author, pages, isRead);
     myLibrary.push(newBook);
+
+    const bookContainer = document.getElementById("bookContainer")
+
+    const bookCard = document.createElement("div");
+    bookCard.classList = "bookCard";
+    bookContainer.appendChild(bookCard);
+
+    const titleDiv = document.createElement("div");
+    const authorDiv = document.createElement("div");
+    const pagesDiv = document.createElement("div");
+    const isReadDiv = document.createElement("div");
+
+    titleDiv.classList = "title";
+    authorDiv.classList = "author";
+    pagesDiv.classList = "pages";
+    isReadDiv.classList = "isRead";
+    
+    bookCard.appendChild(titleDiv);
+    bookCard.appendChild(authorDiv);
+    bookCard.appendChild(pagesDiv);
+    bookCard.appendChild(isReadDiv);
+
+    titleDiv.textContent = newBook.title;
+    authorDiv.textContent = newBook.author;
+    pagesDiv.textContent = newBook.pages + " pages";
+    if (newBook.isRead) {
+      isReadDiv.textContent = "Already read";
+    }
+    else {
+      isReadDiv.textContent = "Not read yet";
+    };
 }
 
 addBookToLibrary("Hi", "John Smith", 133, false);
-addBookToLibrary("Hello World", "Aidan Smith", 133, true);
-console.log(myLibrary)
+addBookToLibrary("Hello World", "Aidan Smith", 153, true);
+addBookToLibrary("Another Book", "Aidan Jones", 463, true);
+
+
+
